76d2472434a07c51d65441228033bd78a0aaecb0
schierlm <schierlm@0978ed96-842d-0410-98a0-d10ac45840b6>
2007-07-13 21:44:28 +0000

    Bookmark tab: export to CSV, import from CSV or PDF

--- jpdftweak/src/jpdftweak/gui/tabs/BookmarkTab.java
+++ jpdftweak/src/jpdftweak/gui/tabs/BookmarkTab.java
@@ -1,14 +1,27 @@
 package jpdftweak.gui.tabs;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import jpdftweak.core.PdfBookmark;
+import jpdftweak.core.PdfInputFile;
 import jpdftweak.core.PdfTweak;
 import jpdftweak.gui.MainForm;
 import jpdftweak.gui.TableComponent;
@@ -20,7 +33,7 @@ import com.lowagie.text.DocumentException;
 public class BookmarkTab extends Tab {
 
 	private JButton load;
-	//private JButton importPdf, importXml, exportXml, filter;
+	private JButton importPDF, importCSV, exportCSV;
 	private TableComponent bookmarks;
 	private JCheckBox changeBookmarks;
 	private final MainForm mainForm;
@@ -43,17 +56,88 @@ public class BookmarkTab extends Tab {
 				appendBookmarks(bm);
 			}
 		});
-		// TODO bookmark buttons
-		//JPanel panel = new JPanel(new GridLayout(1, 4));
-		//panel.add(importPdf =new JButton("Import from PDF"));
-		//panel.add(importXml =new JButton("Import from CSV"));
-		//panel.add(exportXml =new JButton("Export to CSV"));
-		//panel.add(filter = new JButton("Filter/Shift"));
-		//add(panel, cc.xyw(1, 2, 2));
+		JPanel panel = new JPanel(new GridLayout(1, 3));
+		panel.add(importPDF =new JButton("Import from PDF"));
+		importPDF.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = mainForm.getPdfChooser();
+				if (chooser.showOpenDialog(mainForm) == JFileChooser.APPROVE_OPTION) {
+					try {
+						PdfInputFile pif = new PdfInputFile(chooser.getSelectedFile(), "");
+						bookmarks.clear();
+						appendBookmarks(pif.getBookmarks(1));
+					} catch (IOException ex) {
+						ex.printStackTrace();
+						JOptionPane.showMessageDialog(mainForm, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
+					}	
+				}
+			}
+		});
+		panel.add(importCSV =new JButton("Import from CSV"));
+		importCSV.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jfc = new JFileChooser();
+				if (jfc.showOpenDialog(mainForm) == JFileChooser.APPROVE_OPTION) {
+					importCSV(jfc.getSelectedFile());
+				}
+			}
+
+		});
+		panel.add(exportCSV =new JButton("Export to CSV"));
+		exportCSV.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jfc = new JFileChooser();
+				if (jfc.showSaveDialog(mainForm) == JFileChooser.APPROVE_OPTION) {
+					File f = jfc.getSelectedFile();
+					if (f.exists()) {
+						if (JOptionPane.showConfirmDialog (mainForm,
+								"Overwrite existing file?","Confirm Overwrite",
+								JOptionPane.YES_NO_OPTION,
+								JOptionPane.QUESTION_MESSAGE) == JOptionPane.NO_OPTION) return;
+					}
+					exportCSV(f);
+				}
+			}
+		});
+		add(panel, cc.xyw(1, 2, 2));
 		add(bookmarks = new TableComponent(new String[] {"Depth", "Open",  "Title", "Page", "Position", "Bold", "Italic", "Options"},
 				new Class[]{ Integer.class, Boolean.class, String.class, Integer.class, String.class, Boolean.class, Boolean.class, String.class},
 				new Object[]{1, false, "", 1, "", false, false, ""}), cc.xyw(1, 3, 2));
 		updateEnabledState();
+	}
+
+	protected void importCSV(File selectedFile) {
+		try {
+			BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(selectedFile), "UTF-8"));
+			String line;
+			List<PdfBookmark> bmks = new ArrayList<PdfBookmark>();
+			while ((line = r.readLine()) != null) {
+				if (line.startsWith("#")) continue;
+				bmks.add(PdfBookmark.parseBookmark(line));
+			}
+			r.close();
+			bookmarks.clear();
+			appendBookmarks(bmks);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(mainForm, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	protected void exportCSV(File selectedFile) {
+		try {
+			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(selectedFile), "UTF-8"));
+			for (int i = 0; i < bookmarks.getRowCount(); i++) {
+				PdfBookmark b = getBookmark(bookmarks.getRow(i));
+				w.write(b.toString());
+				w.newLine();
+			}
+			w.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(mainForm, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
+		}	
 	}
 
 	protected void appendBookmarks(List<PdfBookmark> bm) {
@@ -76,10 +160,9 @@ public class BookmarkTab extends Tab {
 
 	protected void updateEnabledState() {
 		load.setEnabled(changeBookmarks.isSelected());
-		//importPdf.setEnabled(changeBookmarks.isSelected());
-		//importXml.setEnabled(changeBookmarks.isSelected());
-		//exportXml.setEnabled(changeBookmarks.isSelected());
-		//filter.setEnabled(changeBookmarks.isSelected());
+		importPDF.setEnabled(changeBookmarks.isSelected());
+		importCSV.setEnabled(changeBookmarks.isSelected());
+		exportCSV.setEnabled(changeBookmarks.isSelected());
 		bookmarks.setEnabled(changeBookmarks.isSelected());
 	}
 
