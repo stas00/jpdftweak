3aa098fb3a3cd4e9d724f8aca784fec29523d972
schierlm <schierlm@0978ed96-842d-0410-98a0-d10ac45840b6>
2009-04-18 12:34:25 +0000

    - Show a red ugly dialog box when a fatal exception occurs

--- trunk/jpdftweak/src/jpdftweak/Main.java
+++ trunk/jpdftweak/src/jpdftweak/Main.java
@@ -1,5 +1,14 @@
 package jpdftweak;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Frame;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
 import jpdftweak.cli.CommandLineInterface;
@@ -13,6 +22,27 @@ public class Main {
 			try {
 				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			} catch (Exception ex) {}
+			Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+				public void uncaughtException(Thread t, Throwable e) {
+					e.printStackTrace();
+					JDialog exceptionDialog = new JDialog((Frame)null, "An unexpected error occurred while running jPDF Tweak.");
+					StringWriter sw = new StringWriter();
+					PrintWriter pw = new PrintWriter(sw);
+					pw.write("Please send this error report to <schierlm@users.sourceforge.net>.\n" +
+							"Try to provide specific information about when this error occured.\n\n" +
+							"jPDF Tweak version: " + VERSION + "\n" +
+							"Java version: " + System.getProperty("java.version") + " (" + System.getProperty("java.vendor")+")\n" +
+						    "Operating System: " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ", " +	System.getProperty("os.arch")+")\n\n");
+					e.printStackTrace(new PrintWriter(sw));
+					JTextArea jta = new JTextArea(sw.toString(), 20, 80);
+					jta.setBackground(new Color(0xff, 0xbb, 0xbb));
+					jta.setEditable(false);
+					exceptionDialog.add(new JScrollPane(jta), BorderLayout.CENTER);
+					exceptionDialog.pack();
+					exceptionDialog.setLocationRelativeTo(null);
+					exceptionDialog.setVisible(true);
+				}
+			});
 			new MainForm();
 		} else {
 			try {
