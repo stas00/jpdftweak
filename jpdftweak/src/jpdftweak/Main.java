83745bcad41f491f6abafc0714a3ea2fe13b9809
schierlm <schierlm@0978ed96-842d-0410-98a0-d10ac45840b6>
2009-07-13 07:25:17 +0000

    Version 0.9.5

--- jpdftweak/src/jpdftweak/Main.java
+++ jpdftweak/src/jpdftweak/Main.java
@@ -15,7 +15,7 @@ import jpdftweak.cli.CommandLineInterface;
 import jpdftweak.gui.MainForm;
 
 public class Main {
-	public static final String VERSION = "0.9";
+	public static final String VERSION = "0.9.5";
 
 	public static void main(String[] args) {
 		if (args.length == 0) {
