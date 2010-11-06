6209d0db640c596d31275424cd1f954aea490b45
schierlm <schierlm@0978ed96-842d-0410-98a0-d10ac45840b6>
2010-11-06 19:11:43 +0000

    prepare 1.0-rc1 release

--- jpdftweak/src/jpdftweak/Main.java
+++ jpdftweak/src/jpdftweak/Main.java
@@ -15,7 +15,7 @@ import jpdftweak.cli.CommandLineInterface;
 import jpdftweak.gui.MainForm;
 
 public class Main {
-	public static final String VERSION = "0.9.6";
+	public static final String VERSION = "1.0-rc1";
 
 	public static void main(String[] args) {
 		if (args.length == 0) {
