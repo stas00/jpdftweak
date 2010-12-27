e67ad890214fc3e4a0a6be1deb480dd0b2d42b2c
schierlm <schierlm@0978ed96-842d-0410-98a0-d10ac45840b6>
2010-12-27 17:22:30 +0000

    Prepare 1.0 release

--- trunk/jpdftweak/src/jpdftweak/Main.java
+++ trunk/jpdftweak/src/jpdftweak/Main.java
@@ -16,7 +16,7 @@ import jpdftweak.cli.CommandLineInterface;
 import jpdftweak.gui.MainForm;
 
 public class Main {
-	public static final String VERSION = "1.0-rc1-dev";
+	public static final String VERSION = "1.0";
 
 	public static void main(String[] args) {
 		String missingLib = findMissingLibName();
